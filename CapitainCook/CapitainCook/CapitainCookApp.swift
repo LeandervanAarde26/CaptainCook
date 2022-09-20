f2933c0677b871c30306d72c6e714a9d1953c52b
LeandervanAarde <200211@virtualwindow.co.za>
2022-09-20 20:00:01 +0200

    Created App
    App has been initialised

---
+++ CapitainCook/CapitainCook/CapitainCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapitainCookApp.swift
+//  CapitainCook
+//
+//  Created by Leander Van Aarde on 2022/09/20.
+//
+
+import SwiftUI
+
+@main
+struct CapitainCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
